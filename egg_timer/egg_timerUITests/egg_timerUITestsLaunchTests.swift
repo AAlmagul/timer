4f28957c1f024981e1e20f830f1a99a79327fcb5
AbdigaliAlmagul <98630900+AAlmagul@users.noreply.github.com>
2024-10-22 20:55:07 +0500

    Add files via upload

---
+++ egg_timer/egg_timerUITests/egg_timerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  egg_timerUITestsLaunchTests.swift
+//  egg_timerUITests
+//
+//  Created by Алмагуль Абдыгали on 22.10.2024.
+//
+
+import XCTest
+
+final class egg_timerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
